717f53b7e8848f087a294a76bc74f15edcc6351c
syaikha rachmah <hi.syaikha@gmail.com>
2021-11-09 18:49:31 +0700

    first commit

---
+++ SyekaMart/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace SyekaMart
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbKode = new System.Windows.Forms.TextBox();
+            this.martsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dbSyeka = new SyekaMart.dbSyeka();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbHarga = new System.Windows.Forms.TextBox();
+            this.tbStok = new System.Windows.Forms.TextBox();
+            this.tbKategory = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kategoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btTambah = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btHapus = new System.Windows.Forms.Button();
+            this.martsTableAdapter = new SyekaMart.dbSyekaTableAdapters.martsTableAdapter();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.martsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbSyeka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(618, 44);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Autumn in November", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SyekaaMart";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kode Brang";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nama Brang";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Harga Brang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kategory Brang";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stok Brang";
+            // 
+            // tbKode
+            // 
+            this.tbKode.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.martsBindingSource, "kode", true));
+            this.tbKode.Location = new System.Drawing.Point(109, 115);
+            this.tbKode.Name = "tbKode";
+            this.tbKode.Size = new System.Drawing.Size(130, 20);
+            this.tbKode.TabIndex = 6;
+            // 
+            // martsBindingSource
+            // 
+            this.martsBindingSource.DataMember = "marts";
+            this.martsBindingSource.DataSource = this.dbSyeka;
+            // 
+            // dbSyeka
+            // 
+            this.dbSyeka.DataSetName = "dbSyeka";
+            this.dbSyeka.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbNama
+            // 
+            this.tbNama.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.martsBindingSource, "nama", true));
+            this.tbNama.Location = new System.Drawing.Point(109, 140);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(130, 20);
+            this.tbNama.TabIndex = 7;
+            // 
+            // tbHarga
+            // 
+            this.tbHarga.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.martsBindingSource, "harga", true));
+            this.tbHarga.Location = new System.Drawing.Point(109, 166);
+            this.tbHarga.Name = "tbHarga";
+            this.tbHarga.Size = new System.Drawing.Size(130, 20);
+            this.tbHarga.TabIndex = 8;
+            // 
+            // tbStok
+            // 
+            this.tbStok.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.martsBindingSource, "stok", true));
+            this.tbStok.Location = new System.Drawing.Point(109, 192);
+            this.tbStok.Name = "tbStok";
+            this.tbStok.Size = new System.Drawing.Size(130, 20);
+            this.tbStok.TabIndex = 9;
+            // 
+            // tbKategory
+            // 
+            this.tbKategory.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.martsBindingSource, "kategory", true));
+            this.tbKategory.Location = new System.Drawing.Point(109, 217);
+            this.tbKategory.Name = "tbKategory";
+            this.tbKategory.Size = new System.Drawing.Size(130, 20);
+            this.tbKategory.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSeaGreen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.kodeDataGridViewTextBoxColumn,
+            this.namaDataGridViewTextBoxColumn,
+            this.hargaDataGridViewTextBoxColumn,
+            this.stokDataGridViewTextBoxColumn,
+            this.kategoryDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.martsBindingSource;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(347, 234);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // kodeDataGridViewTextBoxColumn
+            // 
+            this.kodeDataGridViewTextBoxColumn.DataPropertyName = "kode";
+            this.kodeDataGridViewTextBoxColumn.HeaderText = "kode";
+            this.kodeDataGridViewTextBoxColumn.Name = "kodeDataGridViewTextBoxColumn";
+            // 
+            // namaDataGridViewTextBoxColumn
+            // 
+            this.namaDataGridViewTextBoxColumn.DataPropertyName = "nama";
+            this.namaDataGridViewTextBoxColumn.HeaderText = "nama";
+            this.namaDataGridViewTextBoxColumn.Name = "namaDataGridViewTextBoxColumn";
+            // 
+            // hargaDataGridViewTextBoxColumn
+            // 
+            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
+            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
+            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
+            // 
+            // stokDataGridViewTextBoxColumn
+            // 
+            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
+            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
+            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
+            // 
+            // kategoryDataGridViewTextBoxColumn
+            // 
+            this.kategoryDataGridViewTextBoxColumn.DataPropertyName = "kategory";
+            this.kategoryDataGridViewTextBoxColumn.HeaderText = "kategory";
+            this.kategoryDataGridViewTextBoxColumn.Name = "kategoryDataGridViewTextBoxColumn";
+            // 
+            // btTambah
+            // 
+            this.btTambah.Location = new System.Drawing.Point(11, 257);
+            this.btTambah.Name = "btTambah";
+            this.btTambah.Size = new System.Drawing.Size(64, 20);
+            this.btTambah.TabIndex = 12;
+            this.btTambah.Text = "Tambah";
+            this.btTambah.UseVisualStyleBackColor = true;
+            this.btTambah.Click += new System.EventHandler(this.btTambah_Click);
+            // 
+            // btEdit
+            // 
+            this.btEdit.Location = new System.Drawing.Point(89, 257);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(64, 20);
+            this.btEdit.TabIndex = 13;
+            this.btEdit.Text = "Edit";
+            this.btEdit.UseVisualStyleBackColor = true;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // btHapus
+            // 
+            this.btHapus.Location = new System.Drawing.Point(174, 257);
+            this.btHapus.Name = "btHapus";
+            this.btHapus.Size = new System.Drawing.Size(64, 20);
+            this.btHapus.TabIndex = 14;
+            this.btHapus.Text = "Hapus";
+            this.btHapus.UseVisualStyleBackColor = true;
+            this.btHapus.Click += new System.EventHandler(this.btHapus_Click);
+            // 
+            // martsTableAdapter
+            // 
+            this.martsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 387);
+            this.Controls.Add(this.btHapus);
+            this.Controls.Add(this.btEdit);
+            this.Controls.Add(this.btTambah);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbKategory);
+            this.Controls.Add(this.tbStok);
+            this.Controls.Add(this.tbHarga);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.tbKode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.martsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbSyeka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbKode;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbHarga;
+        private System.Windows.Forms.TextBox tbStok;
+        private System.Windows.Forms.TextBox tbKategory;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btTambah;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btHapus;
+        private dbSyeka dbSyeka;
+        private System.Windows.Forms.BindingSource martsBindingSource;
+        private dbSyekaTableAdapters.martsTableAdapter martsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hargaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stokDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kategoryDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ SyekaMart/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyekaMart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbSyeka.marts' table. You can move, or remove it, as needed.
+            this.martsTableAdapter.Fill(this.dbSyeka.marts);
+            martsBindingSource.DataSource = this.dbSyeka.marts;
+        }
+
+        private void btTambah_Click(object sender, EventArgs e)
+        {
+            try {
+                this.dbSyeka.marts.AddmartsRow(this.dbSyeka.marts.NewmartsRow());
+                martsBindingSource.MoveLast();
+                martsBindingSource.EndEdit();
+                martsTableAdapter.Update(this.dbSyeka.marts);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                martsBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                martsBindingSource.EndEdit();
+                martsTableAdapter.Update(this.dbSyeka.marts);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                martsBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btHapus_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("kamu akan menghapus item ini?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                martsBindingSource.RemoveCurrent();
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
